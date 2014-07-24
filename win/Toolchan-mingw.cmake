9722dc3a8c6f72c4b4d6d05f99dad34646d83279
Adam <Adam@anope.org>
2014-07-24 08:31:12 -0400

    cleanup toolchain file

--- win/Toolchan-mingw.cmake
+++ win/Toolchan-mingw.cmake
@@ -1,13 +1,14 @@
 SET(CMAKE_SYSTEM_NAME Windows)
 
-SET(CMAKE_C_COMPILER /usr/bin/i686-w64-mingw32-gcc)
-SET(CMAKE_CXX_COMPILER /usr/bin/i686-w64-mingw32-g++)
-SET(CMAKE_RC_COMPILER /usr/bin/i686-w64-mingw32-windres)
+SET(CMAKE_C_COMPILER ${TOOLCHAIN_PREFIX}-gcc)
+SET(CMAKE_CXX_COMPILER ${TOOLCHAIN_PREFIX}-g++)
+SET(CMAKE_RC_COMPILER ${TOOLCHAIN_PREFIX}-windres)
 
-set( CMAKE_SHARED_LIBRARY_LINK_C_FLAGS "" )
-set( CMAKE_SHARED_LIBRARY_LINK_CXX_FLAGS "" )
+# http://www.cmake.org/pipermail/cmake/2007-October/016784.html
+set(CMAKE_SHARED_LIBRARY_LINK_C_FLAGS "")
+set(CMAKE_SHARED_LIBRARY_LINK_CXX_FLAGS "")
 
-#SET(CMAKE_C_FLAGS "-std=c++11")
+# http://stackoverflow.com/questions/19866926/cmake-toolchain-file-wont-use-my-custom-cflags
 SET(CMAKE_CXX_FLAGS "-DMINGW -std=c++11" CACHE STRING "" FORCE)
 
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
