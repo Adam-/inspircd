890912b0804bc0d3effd805850d358d0b6ccb8b0
Adam <Adam@anope.org>
2015-06-03 12:42:41 -0400

    add wrapper

---
+++ src/modules/m_python/wrapper.cpp
@@ -0,0 +1,8 @@
+
+/* $CompileFlags: -w -frtti pkgconfincludes("python","/Python.h","") */
+
+#define SWIG
+#define SWIG_TABLE_TYPE "inspircd"
+#define SWIG_DIRECTOR_NORTTI
+
+#include "inspircd_wrap.cxx"
