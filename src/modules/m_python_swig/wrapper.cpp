9e99630d7048a5de20dabc8dd09ec42075ae03d9
Adam <Adam@anope.org>
2015-06-03 16:15:19 -0400

    table type -> type table

--- src/modules/m_python_swig/wrapper.cpp
+++ src/modules/m_python_swig/wrapper.cpp
@@ -1,6 +1,6 @@
 
 /* $CompileFlags: exec("swig -python -c++  -cpperraswarn  -I../include ../src/modules/m_python_swig/inspircd.i") -w -frtti -fvisibility=default pkgconfincludes("python","/Python.h","") */
 
-#define SWIG_TABLE_TYPE "inspircd"
+#define SWIG_TYPE_TABLE "inspircd"
 
 #include "inspircd_wrap.cxx"
